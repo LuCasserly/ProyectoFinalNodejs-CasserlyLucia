c5a8b8abcf3f48c1d0c823d0632ae88a1513cd1b
lcasserly <luciacasserly@gmail.com>
2025-07-18 23:29:38 -0300

    Primer commit

---
+++ index.js
@@ -0,0 +1,19 @@
+import "dotenv/config";
+import express from "express";
+import productsRouter from "./src/routes/products.router.js";
+import authRouter from "./src/routes/auth.router.js";
+/*import notFoundMiddleware from "./src/middlewares/notFound.js";*/
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Bienvenidos a la tienda virtual" });
+});
+
+app.use("/api/products", productsRouter);
+app.use("/auth", authRouter);
+
+app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
